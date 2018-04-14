75778cbce11f5c22b1309278005ca7bc9f00dced
Fredy <jfr3dy90@gmail.com>
2018-04-13 21:13:07 -0500

    Faltaportales

---
+++ Sapiencia/Assets/Main/Scripts/Character.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Character", menuName = "Model")]
+public class Character : ScriptableObject
+{
+    public GameObject model;
+	
+}
