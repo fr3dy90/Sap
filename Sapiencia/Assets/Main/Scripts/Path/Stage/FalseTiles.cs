549427329d13cf501beb97b45bd11e781d59c9b3
fr3dy <jfr3dy90@gmail.com>
2018-04-16 15:55:56 -0500

    Base

--- Sapiencia/Assets/Main/Scripts/Path/Stage/FalseTiles.cs
+++ Sapiencia/Assets/Main/Scripts/Path/Stage/FalseTiles.cs
@@ -2,22 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FalseTiles : MonoBehaviour {
+public class FalseTiles : MonoBehaviour
+{
 
     public Texture2D map;
-    public Sprite sprite;
-    public float offsetX;
-    public float offsetY;
+
+    public Sprite sprites;
+    public Color colors;
+    public float offset_x;
+    public float offset_y;
     public float order;
-    public string nameLayer;
 
-    public GameObject falseGrid()
+    float offsetH;
+
+
+
+    public GameObject grid()
     {
-        GameObject go = new GameObject("FalseGrid")
+        GameObject go = new GameObject("FalseGrid");
+        go.isStatic = true;
+        for (int i = 0; i < map.width; i++)
         {
-
+            for (int j = 0; j < map.height; j++)
+            {
+                Color pixelColor = map.GetPixel(i, j);
+                if (pixelColor.a != 0)
+                {
+                    GameObject tile = new GameObject(i.ToString() + "_" + j.ToString());
+                    tile.transform.parent = go.transform;
+                    SpriteRenderer sr = tile.AddComponent<SpriteRenderer>();
+                    if (colors.Equals(pixelColor))
+                    {
+                        sr.sprite = sprites;
+                        offsetH = 4;
+                    }
+                    tile.transform.position = new Vector2((i * offset_x) + (j * offset_x), ((i * (-offset_y)) + (j * offset_y) + offsetH));
+                    sr.sortingLayerName = "Walls";
+                    sr.sortingOrder = (int)(tile.transform.position.y * order);
+                    tile.isStatic = true;
+                }
+            }
         }
+        return go;
     }
 
-
+    private void Awake()
+    {
+        grid();
+    }
 }
